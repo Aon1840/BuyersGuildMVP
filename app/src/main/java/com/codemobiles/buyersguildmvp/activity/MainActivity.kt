9d04d2cba1e2669965341e6edc5c005a98105807
Attapon Peungsook <aonattapon1840.ap@gmail.com>
2019-09-03 17:45:30 +0700

    Add mobileListFragment and favouriteListFragment

--- app/src/main/java/com/codemobiles/buyersguildmvp/activity/MainActivity.kt
+++ app/src/main/java/com/codemobiles/buyersguildmvp/activity/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.codemobiles.buyersguildmvp.activity
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.codemobiles.buyersguildmvp.R
 import com.codemobiles.buyersguildmvp.ui.main.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,11 +18,6 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
