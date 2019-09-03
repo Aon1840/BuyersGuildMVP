901a24490474aacc3a5cc7011b0252d8ba0e763b
Attapon Peungsook <aonattapon1840.ap@gmail.com>
2019-09-04 01:32:25 +0700

    Add MainApplication class

---
+++ app/src/main/java/com/codemobiles/buyersguildmvp/application/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.codemobiles.buyersguildmvp.application
+
+import android.app.Application
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
